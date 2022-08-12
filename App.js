24f869a0c86d349445cdf0167df486592899d139
santiserena <santiserena@outlook.com.ar>
2022-08-12 14:00:58 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
+
+
+import { Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
+
+function MySongBook() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>
+        <h1>List:</h1>
+        -aaaaaa <br/>
+        -bbbbbb <br/>
+        -cccccc <br/>
+      </Text>
+    </View>
+  );
+}
+
+function Explorer() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>
+        Soy el explorador
+      </Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>soy el Setter</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="My Songbook" component={MySongBook} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Explorer" component={Explorer} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+/* import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>SongBook</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +72,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+}); */
