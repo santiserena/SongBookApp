09b794a26dcef338acfb3f67eab74a16423d6f43
santiserena <santiserena@outlook.com.ar>
2022-08-12 18:33:28 -0300

    elements added

--- components/MySongBook.jsx
+++ components/MySongBook.jsx
@@ -1,13 +1,36 @@
-import { View, Text } from "react-native-web";
+import { View, Text, TextInput } from "react-native";
+import { Button } from "react-native-web";
+import { useState } from "react";
 
 export default function MySongBook() {
+  
+  const [find, setFind] = useState('')
+  
+  
+    const onChangeText = (ev) => {
+      setFind(ev);
+    };
+
+    const search = () => {
+      console.log("soy el estado", find);
+    };
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Button title="+" />
+      <br/><br/>
+
+      <TextInput
+        onChangeText={(ev) => onChangeText(ev)}
+        placeholder="write.."
+      />
+      <Button onPress={() => search()} title="Search by artist or song name" />
+
       <Text>
         <h1>List:</h1>
         -aaaaaa <br />
         -bbbbbb <br />
-        -cccccc 
+        -cccccc
       </Text>
     </View>
   );
