baf03a43f9f69a3a48199e04db5c3834dbdb1e0f
santiserena <santiserena@outlook.com.ar>
2022-08-15 16:16:41 -0300

    Gets songs searched in the Created component

--- components/Creator/Creator.jsx
+++ components/Creator/Creator.jsx
@@ -1,34 +1,52 @@
 import { useState } from "react";
 import axios from "react-native-axios";
-import { View, Text, Button, TextInput, ActivityIndicator } from "react-native-web";
+//import { View, Text, Button, TextInput, ActivityIndicator } from "react-native-web";
 import {API_KEY} from "@env"
+import { Image, ScrollView, View, Text, Button, TextInput, ActivityIndicator } from "react-native";
 
 
 export default function Creator() {
   const [toSearch, setToSearch] = useState("");
+  const [found, setFound] = useState([]);
 
   const search = async () => {
-    let res = await axios.get(
-      `https://api.happi.dev/v1/music?q=Get%20Lucky&limit=&apikey=${API_KEY}&type=:type&lyrics=1`
-    );
-    console.log("soy el buscador", res.data);
+    let urlMaker = toSearch.split(" ");
+    urlMaker = urlMaker.join("%20");
+    urlMaker = `https://api.happi.dev/v1/music?q=${urlMaker}&limit=&apikey=${API_KEY}&type=:type&lyrics=1`;
+
+    let res = await axios.get(urlMaker);
+    if (res.data.success) setFound(res.data.result);
   };
 
   return (
-    
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <ScrollView /* style={{ flex: 1, justifyContent: "center", alignItems: "center" }} */
+    >
       <Button
         onPress={() => navigation.navigate("My Songbook")}
         title="Go back to my song book"
       />
       {/* <ActivityIndicator size='large'/> */}
 
-      <Text>First, search by name or artist, the lyrics of a song:</Text>
+      <Text>First, search the song. You can add the artist too</Text>
       <TextInput
         onChangeText={(ev) => setToSearch(ev)}
         placeholder="michael jackson"
       />
       <Button onPress={() => search()} title="See results" />
-    </View>
+
+      <Text>LISTA:</Text>
+      <br />
+      {found?.map((el, index) => (
+        <View key={index}>
+          <Text>Artist: {el.artist}</Text>
+          <Text>Song: {el.track}</Text>
+          <Text>Album: {el.album}</Text>
+          <Image
+            source={{ uri: `${el.cover}?apikey=${API_KEY}` }}
+            style={{ width: 400, height: 400 }}
+          />
+        </View>
+      ))}
+    </ScrollView>
   );
 }
--- config.js
+++
@@ -1,3 +0,0 @@
-import {API_URL} from "@env"
-
-console.log('askaasxasxs', API_URL);
