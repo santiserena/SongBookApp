357fc8e7b812a9c12993b64f5e79587fdd8fb6bb
santiserena <santiserena@outlook.com.ar>
2022-09-21 18:10:09 -0300

    login component

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import MySongBook from "./components/MySongBook/MySongBook";
 import Explorer from "./components/Explorer/Explorer";
 import SettingsScreen from "./components/Settings/Settings";
+import Login from "./components/Login";
 //import Creator from "./components/Creator/Creator";
 //import Creator2 from "./components/Creator/Creator2";
 //import LyricsManually from "./components/Creator/LyricsManually";
@@ -18,45 +19,47 @@ export default function App() {
   return (
     <Provider store = {store}>
 
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-        >
-        <Tab.Screen name="My Songbook" component={MySongBook} />
-        <Tab.Screen name="Explorer" component={Explorer} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-        <Tab.Screen name="CreationScreens" component={CreationScreens} />
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+          >
+          <Tab.Screen name="Login" component={Login} />
+          <Tab.Screen name="My Songbook" component={MySongBook} />
+          <Tab.Screen name="Explorer" component={Explorer} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+          <Tab.Screen name="CreationScreens" component={CreationScreens} />
 
-        {/* <Tab.Screen
-          name="LyricsManually"
-          component={LyricsManually}
-          options={{
-            tabBarStyle: { display: "none" },
-            tabBarButton: () => null,
-          }}
-        /> */}
-       {/*  <Tab.Screen
-          name="Creator"
-          component={Creator}
-          options={{
-            // taking the tab out 
-            tabBarStyle: { display: "none" },
-            // taking the icon out 
-            tabBarButton: () => null,
-          }}
-        /> */}
-         {/*  <Tab.Screen
-            name="Creator2"
-            component={Creator2}
+          {/* <Tab.Screen
+            name="LyricsManually"
+            component={LyricsManually}
             options={{
               tabBarStyle: { display: "none" },
               tabBarButton: () => null,
             }}
           /> */}
-      </Tab.Navigator>
-    </NavigationContainer>
-          </Provider>
+        {/*  <Tab.Screen
+            name="Creator"
+            component={Creator}
+            options={{
+              // taking the tab out 
+              tabBarStyle: { display: "none" },
+              // taking the icon out 
+              tabBarButton: () => null,
+            }}
+          /> */}
+          {/*  <Tab.Screen
+              name="Creator2"
+              component={Creator2}
+              options={{
+                tabBarStyle: { display: "none" },
+                tabBarButton: () => null,
+              }}
+            /> */}
+        </Tab.Navigator>
+      </NavigationContainer>
+
+    </Provider>
   );
 }
---
+++ components/Login.jsx
@@ -0,0 +1,12 @@
+import { View, Text, TextInput, Button, ScrollView, StyleSheet } from "react-native";
+
+export default function Login() {
+
+
+  return (
+    <ScrollView>   
+        <Text>{'\n\n\n\n\n'}este es el mail:</Text>
+    </ScrollView>
+  );
+}
+
